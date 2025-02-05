30c50b3bf4f95645b42e6728e789fb9a0a406390
Dionathan Cordova <dionathan.cordova@gmail.com>
2025-02-05 01:26:49 -0300

    feat: adicionando arquivos para doc das rotas

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 // src/server.js
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require( "../swagger.json");
+const checkHorizonStatus = require("./middlewares/check-horizon-status");
+
 const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
+app.use(checkHorizonStatus);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`📜 Documentação disponível em http://localhost:${PORT}/api-docs`);
 });
